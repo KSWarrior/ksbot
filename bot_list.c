cb6e58477ce961a1988d3bc2da2bbc198b0d3a35
KSWarrior <kswarriorpro@gmail.com>
2025-06-18 12:28:18 +0530

    Update bot_list.c

--- bot_list.c
+++ bot_list.c
@@ -1,1 +1,5 @@
 npx pm2 start bot_1.js --name ks_warrior_pro_1
+npx pm2 start bot_2.js --name ks_warrior_pro_2
+npx pm2 start bot_3.js --name ks_warrior_pro_3
+npx pm2 start bot_4.js --name ks_warrior_pro_4
+npx pm2 start bot_5.js --name ks_warrior_pro_5
