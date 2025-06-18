33275e73b95412e97e8f59678ca7305e272f72ad
KSWarrior <kswarriorpro@gmail.com>
2025-06-18 12:27:14 +0530

    Update bot_list.c

--- bot_list.c
+++ bot_list.c
@@ -1,5 +1,1 @@
-bot_1.js ks_warrior_pro kswarrior.aternos.me:53086
-bot_2.js username ip
-bot_3.js username ip
-bot_4.js username ip
-bot_5.js username ip
+npx pm2 start bot_1.js --name ks_warrior_pro_1
